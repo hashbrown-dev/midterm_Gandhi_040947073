e9dd67ada3fbc0a9d5d4185b56098d32a1c12b10
Harsh Gandhi <gand0024@algonquinlive.com>
2018-11-13 19:03:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# midterm_Gandhi_040947073
---
+++ js/script.js
@@ -0,0 +1,4 @@
+function changePicture() {
+    window.open("https://picsum.photos/200/300/?gravity=east", "Thank you", "width=200, height=100");
+    window.open('https://picsum.photos/200/300/?gravity=east', 'yourWindowName', 'width=200,height=150);
+}
